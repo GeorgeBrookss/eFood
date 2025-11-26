8e3789c7b70b31b613f097a76293c70d772a3ccb
George Hughes <168946733+GeorgeBrookss@users.noreply.github.com>
2025-11-26 16:13:32 -0300

    Update routers.tsx

--- src/routers.tsx
+++ src/routers.tsx
@@ -5,8 +5,8 @@ import LaDolce from './components/Pages/LaDolce/LaDolce'
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/restaurante/:id/" element={<LaDolce />} />
-    <Route path="/restaurante/:id/produto/:idProduto" element={<LaDolce />} />
+    <Route path="/restaurantes/:id/" element={<LaDolce />} />
+    <Route path="/restaurantes/:id/produto/:idProduto" element={<LaDolce />} />
   </Routes>
 )
 
