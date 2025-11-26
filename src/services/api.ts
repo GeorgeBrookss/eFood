758aeac3ddedc24681958fca0b3710fb50d08a93
George Hughes <168946733+GeorgeBrookss@users.noreply.github.com>
2025-11-26 16:34:42 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,14 @@ interface ApiRestauranteResponse {
   avaliacao: number
   descricao: string
   capa: string
-  cadapio: any[]
+  cardapio: {
+    foto: string
+    preco: number
+    id: number
+    nome: string
+    descricao: string
+    porcao: string
+  }[]
 }
 
 const api = createApi({
@@ -18,18 +25,22 @@ const api = createApi({
     baseUrl: 'https://api-ebac.vercel.app/api/efood/'
   }),
   endpoints: (builder) => ({
-    getRestaurantes: builder.query<Restaurante[], void>({
+    etRestaurantes: builder.query<Restaurante[], void>({
       query: () => 'restaurantes',
       transformResponse: (rawResult: ApiRestauranteResponse[]) => {
         return rawResult.map((item) => ({
           id: item.id,
-          preco: 0,
-          nome: item.titulo,
-          title: item.titulo,
+          titulo: item.titulo,
+          destacado: item.destacado,
+          tipo: item.tipo,
+          avaliacao: item.avaliacao,
           descricao: item.descricao,
           foto: item.capa,
-          infos: [item.tipo],
           nota: item.avaliacao,
+          nome: item.titulo,
+          foto: item.capa,
+          infos: [item.tipo], 
+          preco: 0,
           redirecionador: `/restaurantes/${item.id}`
         }))
       }
@@ -38,18 +49,33 @@ const api = createApi({
       query: (id) => `restaurantes/${id}`,
       transformResponse: (rawResult: ApiRestauranteResponse) => ({
         id: rawResult.id,
-        preco: 0,
-        nome: rawResult.titulo,
-        title: rawResult.titulo,
+        titulo: rawResult.titulo,
+        destacado: rawResult.destacado,
+        tipo: rawResult.tipo,
+        avaliacao: rawResult.avaliacao,
         descricao: rawResult.descricao,
-        foto: rawResult.capa,
-        infos: [rawResult.tipo],
+        capa: rawResult.capa,
         nota: rawResult.avaliacao,
-        redirecionador: `/restaurantes/${rawResult.id}`
+        nome: rawResult.titulo,
+        foto: rawResult.capa, 
+        infos: [rawResult.tipo],
+        preco: 0,
+        redirecionador: `/restaurantes/${rawResult.id}`,
+        cardapio: rawResult.cardapio
       })
     }),
     getPratos: builder.query<PratosMold[], string>({
-      query: (restauranteId) => `restaurantes/${restauranteId}/cardapio`
+      query: (restauranteId) => `restaurantes/${restauranteId}`,
+      transformResponse: (rawResult: ApiRestauranteResponse) => {
+        return rawResult.cardapio.map(item => ({
+            id: item.id,
+            nome: item.nome,
+            descricao: item.descricao,
+            foto: item.foto,
+            preco: item.preco,
+            porcao: item.porcao
+        }))
+      }
     })
   })
 })
